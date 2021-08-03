ee51e474f0fa74748575bb1aa689d10881b8d7f1
BRUNA PEREIRA <bruna.pereira2@alunos.sc.senac.br>
2021-08-02 21:40:01 -0300

    atualização

---
+++ Aula 003 Logica de Programacao conceito de algoritmo algoritmos estruturados/Tamplatebranco/src/tamplatebranco/Tamplatebranco.java
@@ -0,0 +1,38 @@
+// Programa teste olamundo
+        
+package tamplatebranco;
+
+/**
+ *
+ * @author bruna.pereira2
+ */
+public class Tamplatebranco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String str1 = "Olá mundo"; 
+        int a = 6;
+        int b = 7;
+        int c = a + b;
+        
+        int d = 1;
+        int e = 2;
+        int f = 3;
+        int g = 4;
+        
+        int h = d + d;
+        int i = d + e;
+        int j = d + f;
+        int k = d + g;
+        System.out.println(str1);
+        System.out.println("6+7 = " + c);
+        System.out.println("1+1 = " + h);
+        System.out.println("1+2 = " + i);
+        System.out.println("1+3 = " + j);
+        System.out.println("1+4 = " + k);
+        
+    }
+    
+}
